c1a83cbdb8962a29b8e03d204a0eb578b46b4159
ongaeshi <ongaeshi0621@gmail.com>
2019-03-06 00:31:11 +0900

    feat: Add doChangeState, doRemove

--- main.js
+++ main.js
@@ -36,8 +36,13 @@ const app = new Vue({
       })
       // Empty form
       comment.value = ''
-
-
+    },
+    doChangeState: function(item) {
+      item.state = item.state ? 0 : 1;
+    },
+    doRemove: function(item) {
+      var index = this.todos.indexOf(item)
+      this.todos.splice(index, 1)
     }
   },
   watch: {
